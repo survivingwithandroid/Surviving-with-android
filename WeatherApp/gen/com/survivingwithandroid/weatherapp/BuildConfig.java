14fedfaf6345d593730d585d3cfecb7082adf8da
Survivingwithandroid <survivingwithandroid@gmail.com>
2013-05-23 22:30:07 +0200

    Initial commit

---
+++ WeatherApp/gen/com/survivingwithandroid/weatherapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.survivingwithandroid.weatherapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ WeatherApp/src/com/survivingwithandroid/weatherapp/JSONWeatherParser.java
@@ -0,0 +1,108 @@
+/**
+ * This is a tutorial source code 
+ * provided "as is" and without warranties.
+ *
+ * For any question please visit the web site
+ * http://www.survivingwithandroid.com
+ *
+ * or write an email to
+ * survivingwithandroid@gmail.com
+ *
+ */
+package com.survivingwithandroid.weatherapp;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.survivingwithandroid.weatherapp.model.Location;
+import com.survivingwithandroid.weatherapp.model.Weather;
+
+/*
+ * Copyright (C) 2013 Surviving with Android (http://www.survivingwithandroid.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class JSONWeatherParser {
+
+	public static Weather getWeather(String data) throws JSONException  {
+		Weather weather = new Weather();
+
+		// We create out JSONObject from the data
+		JSONObject jObj = new JSONObject(data);
+		
+		// We start extracting the info
+		Location loc = new Location();
+		
+		JSONObject coordObj = getObject("coord", jObj);
+		loc.setLatitude(getFloat("lat", coordObj));
+		loc.setLongitude(getFloat("lon", coordObj));
+		
+		JSONObject sysObj = getObject("sys", jObj);
+		loc.setCountry(getString("country", sysObj));
+		loc.setSunrise(getInt("sunrise", sysObj));
+		loc.setSunset(getInt("sunset", sysObj));
+		loc.setCity(getString("name", jObj));
+		weather.location = loc;
+		
+		// We get weather info (This is an array)
+		JSONArray jArr = jObj.getJSONArray("weather");
+		
+		// We use only the first value
+		JSONObject JSONWeather = jArr.getJSONObject(0);
+		weather.currentCondition.setWeatherId(getInt("id", JSONWeather));
+		weather.currentCondition.setDescr(getString("description", JSONWeather));
+		weather.currentCondition.setCondition(getString("main", JSONWeather));
+		weather.currentCondition.setIcon(getString("icon", JSONWeather));
+		
+		JSONObject mainObj = getObject("main", jObj);
+		weather.currentCondition.setHumidity(getInt("humidity", mainObj));
+		weather.currentCondition.setPressure(getInt("pressure", mainObj));
+		weather.temperature.setMaxTemp(getFloat("temp_max", mainObj));
+		weather.temperature.setMinTemp(getFloat("temp_min", mainObj));
+		weather.temperature.setTemp(getFloat("temp", mainObj));
+		
+		// Wind
+		JSONObject wObj = getObject("wind", jObj);
+		weather.wind.setSpeed(getFloat("speed", wObj));
+		weather.wind.setDeg(getFloat("deg", wObj));
+		
+		// Clouds
+		JSONObject cObj = getObject("clouds", jObj);
+		weather.clouds.setPerc(getInt("all", cObj));
+		
+		// We download the icon to show
+		
+		
+		return weather;
+	}
+	
+	
+	private static JSONObject getObject(String tagName, JSONObject jObj)  throws JSONException {
+		JSONObject subObj = jObj.getJSONObject(tagName);
+		return subObj;
+	}
+	
+	private static String getString(String tagName, JSONObject jObj) throws JSONException {
+		return jObj.getString(tagName);
+	}
+
+	private static float  getFloat(String tagName, JSONObject jObj) throws JSONException {
+		return (float) jObj.getDouble(tagName);
+	}
+	
+	private static int  getInt(String tagName, JSONObject jObj) throws JSONException {
+		return jObj.getInt(tagName);
+	}
+	
+}
