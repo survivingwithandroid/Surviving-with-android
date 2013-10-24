6e7d849fe770538ea347e54a5e2a5e37d1eadf9c
Surviving with android <survivingwithandroid@gmail.com>
2013-10-24 21:55:50 +0200

    Update MainActivity.java
    Temp bug fix. Thx to blog readers!

--- WeatherApp/src/com/survivingwithandroid/weatherapp/MainActivity.java
+++ WeatherApp/src/com/survivingwithandroid/weatherapp/MainActivity.java
@@ -113,11 +113,11 @@ public class MainActivity extends Activity {
 			
 			cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
 			condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
-			temp.setText("" + Math.round((weather.temperature.getTemp() - 275.15)) + "°C");
+			temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "ï¿½C");
 			hum.setText("" + weather.currentCondition.getHumidity() + "%");
 			press.setText("" + weather.currentCondition.getPressure() + " hPa");
 			windSpeed.setText("" + weather.wind.getSpeed() + " mps");
-			windDeg.setText("" + weather.wind.getDeg() + "°");
+			windDeg.setText("" + weather.wind.getDeg() + "ï¿½");
 				
 		}
 
