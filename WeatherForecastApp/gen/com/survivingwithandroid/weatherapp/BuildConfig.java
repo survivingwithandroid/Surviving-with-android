d71f91342dbe68e33a2909811c92b76a370e5754
Surviving with android <survivingwithandroid@gmail.com>
2014-02-08 22:10:11 +0100

    Moved projects in Swa-app

--- WeatherForecastApp/gen/com/survivingwithandroid/weatherapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.survivingwithandroid.weatherapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
